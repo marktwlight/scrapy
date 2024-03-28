37342c589ceb2f661a069b0d6a69fb7e7c3f6d94
marktwlight <marktwin136@gmail.com>
2024-03-28 11:12:28 +0400

    跳过最后一句话

--- getContentByUrl/spinner.py
+++ getContentByUrl/spinner.py
@@ -12,25 +12,28 @@ import random
 
 def get_primary_synonym(word):
     synonyms = []
-    for synset in wn.synsets(word,lang='por'):
-    
+    for synset in wn.synsets(word, lang='por'):
+
         for lemma in synset.lemmas('por'):
-            
+
             synonyms.append(lemma.name())
-            
+
             break  # 获取第一个同义词即可
     return synonyms
+
 
 def preprocess_text(text):
     sentences = sent_tokenize(text, language='portuguese')  # 分句
     processed_sentences = []
-    for sentence in sentences:
+    for i, sentence in enumerate(sentences):
+        if i == len(sentences) - 1:  # 如果是最后一句话，则跳过
+            continue
         words = word_tokenize(sentence, language='portuguese')  # 分词
         new_words = []
         for word in words:
             synonyms = get_primary_synonym(word)
             if synonyms:
-                new_word = random.choice(synonyms)  # 随机近义词
+                new_word = random.choice(synonyms)  # 随机选择一个同义词
                 new_words.append(new_word)
             else:
                 new_words.append(word)
@@ -40,8 +43,9 @@ def preprocess_text(text):
     return processed_text
 
 # 对文本进行词汇替换和句子重组
+
+
 def transform_text(text):
     transformed_text = preprocess_text(text)
-    rate = similiarRate.getSimilarity(text,transformed_text)
+    rate = similiarRate.getSimilarity(text, transformed_text)
     return transformed_text
-
