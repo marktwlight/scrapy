f759d9eee4812ab27d289a38991aa7e35ecc4652
marktwlight <marktwin136@gmail.com>
2024-04-02 17:23:56 +0400

    获取到每一篇的title, artilce

--- cnn/content.py
+++ cnn/content.py
@@ -1,10 +1,20 @@
 import links
 from DrissionPage import WebPage
-
+from excel import parseContentToExcel
 article_links = links.links
 
 page = WebPage()
 page.change_mode('s')
 for link in article_links:
     page.get(link)
-    print(page.html)
+    # title
+    # article
+    title = page.ele('.post__title').text
+    articleContainer = page.ele('.post__content')
+    paragraphs = articleContainer.children('tag:p')
+    artilce = ''
+    for paragraph in paragraphs:
+        artilce += paragraph.text
+    print('标题------', title)
+    print('文章------', artilce)
+    # parseContentToExcel(title, artilce)
---
+++ cnn/excel.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import spinner
+from datetime import datetime
+
+def getDate():
+    # 获取当前日期
+    current_date = datetime.now()
+
+    # 格式化日期为 YYYY-MM-DD 样式
+    formatted_date = current_date.strftime('%Y-%m-%d')
+
+    return formatted_date
+def parseContentToExcel(htmlContent, category):
+   
+    # 提取标题
+    title = soup.find("h1", class_="content-head__title").text.strip()
+
+    new_title = spinner.transform_text(title, False)
+    # 标题相似度
+    # title_rate = similiarRate.getSimilarity(title,new_title)
+    # 判断相似值，
+    article_body = ""
+    # 提取正文内容
+    paragraphs = soup.find_all("p", class_="content-text__container")
+    for paragraph in paragraphs:
+        # 忽略所有子元素，只获取纯文本内容
+        text = paragraph.text.strip()
+        article_body += text
+
+    # 调用伪原创方法
+    # git
+    new_article_body = spinner.transform_text(article_body, True)
+
+    # 输出标题和正文内容
+    print(category, "标题:", new_title)
+    print("\n", category, "正文内容:", new_article_body)
+    # 获取当前文件的绝对路径
+    current_file_path = os.path.abspath(__file__)
+
+    # 获取当前文件所在的文件夹路径
+    current_folder = os.path.dirname(current_file_path)
+
+    filename = getDate() + " " + category+'.xlsx'
+
+    absolute_path = os.path.join(current_folder, filename)
+
+    # 创建DataFrame
+    data = pd.DataFrame(
+        {'文章标题（不能重复）': [new_title], '文章内容': [new_article_body]})
+#     data.dropna(axis=0, inplace=True)
+
+    if not os.path.exists(absolute_path):
+        data.to_excel(absolute_path, index=False, sheet_name='alizhizhuchi')
+    else:
+
+        with pd.ExcelFile(absolute_path) as xls:
+            if 'alizhizhuchi' in xls.sheet_names:
+                with pd.ExcelWriter(absolute_path, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+                    data.to_excel(writer, index=False, sheet_name='alizhizhuchi',
+                                  startrow=writer.sheets['alizhizhuchi'].max_row, header=False)
